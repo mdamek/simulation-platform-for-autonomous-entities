0543f3161eb0c4cd28bafba754337a14ce7e1887
Marcin <marcind70@gmail.com>
2020-06-13 23:03:28 +0200

    Add painter

--- Visualisations/FillPanelPixels.ts
+++ Visualisations/FillPanelPixels.ts
@@ -1,35 +1,26 @@
-import { LedMatrix, GpioMapping, LedMatrixUtils, PixelMapperType } from 'rpi-led-matrix';
 import { performance } from 'perf_hooks';
-const matrixRGB = new LedMatrix(
-  {
-    ...LedMatrix.defaultMatrixOptions(),
-    rows: 16,
-    cols: 32,
-    chainLength: 2,
-    hardwareMapping: GpioMapping.RegularPi1,
-    pixelMapperConfig: LedMatrixUtils.encodeMappers({ type: PixelMapperType.U })
-  },
-  {
-    ...LedMatrix.defaultRuntimeOptions(),
-    gpioSlowdown:4,
-  }
-);
+import { Painter } from './Painter';
+import { Color} from 'rpi-led-matrix';
 
-console.log(matrixRGB.width())
-console.log(matrixRGB.height())
+
 function getRndInteger(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 let c = 0;
 let start = performance.now();
+let painter = new Painter();
+
 while(c < 1200){
-  let color = {r: getRndInteger(0,255), g: getRndInteger(0,255), b: getRndInteger(0,255)}
-  for(let i = 0; i < matrixRGB.width(); i++){
-    for(let j = 0; j < matrixRGB.height(); j++){
-      matrixRGB.fgColor(color).setPixel(i,j);
+  let color : Color = {r: getRndInteger(0,255), g: getRndInteger(0,255), b: getRndInteger(0,255)}
+  
+  let map : Color[][] = new Array(32).fill(new Array(32));
+  
+  for(let i = 0; i < map.length; i++){
+    for(let j = 0; j < map[0].length; j++){
+        map[i][j] = color;
     }
-  }  
-  matrixRGB.sync()
+  }
+  painter.Paint(map);
   c++;
 }
 let stop = performance.now();
---
+++ Visualisations/Painter.ts
@@ -0,0 +1,35 @@
+import { LedMatrix, LedMatrixInstance, Color } from "rpi-led-matrix";
+import { matrixOptions, runtimeOptions } from './_config';
+
+function findDim(a : Color[][]){
+    let mainLen = a.length;
+    let subLen = a[0].length;
+ 
+    for(let i=0; i < mainLen; i++){
+        let len = a[i].length;
+        subLen = (len < subLen ? len : subLen);
+    }
+
+    return [mainLen, subLen];
+};
+
+export class Painter{
+    matrix: LedMatrixInstance;
+    constructor(){
+        this.matrix = new LedMatrix(matrixOptions, runtimeOptions);
+    }
+    Paint(colorMap : Array<Array<Color>>) : void {
+        let dimensions = findDim(colorMap)
+        if(dimensions[0] < this.matrix.width() || 
+           dimensions[1] < this.matrix.height()){
+            throw new Error("Cannot paint " + dimensions + " on " + [this.matrix.width(), this.matrix.height()])
+        }
+        this.matrix.clear()
+        for(let i = 0; i < this.matrix.width(); i++){
+            for(let j = 0; j < this.matrix.height(); j++){
+                this.matrix.fgColor(colorMap[i][j]).setPixel(j,i);
+            }
+        }
+        this.matrix.sync();
+    }
+}
