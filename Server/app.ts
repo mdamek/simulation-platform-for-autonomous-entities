95199b4879cf0eef13c0e158d4a1df36121ecc6b
mdamek <marcind70@gmail.com>
2021-04-05 01:33:26 +0200

    add clear

--- Server/app.ts
+++ Server/app.ts
@@ -4,45 +4,30 @@ import { PrintOwnText } from "../Visualisations/PrintText";
 import bodyParser from "body-parser";
 import { XinukIteration } from "../Models/XinukInterfaces";
 import { ConvertBodyToXinukIteration } from "./convertionHelpers";
-import { NextPixelsOn } from "../Visualisations/NextPixelsOn";
+import { FillPanelPixels } from "../Visualisations/FillPanelPixels";
 
 const PORT = 8000;
 
 const app: Application = express();
-app.use(bodyParser.json({ limit: "100000mb" }));
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    limit: "100000mb",
-    extended: true,
-    parameterLimit: 500000000,
+    extended: true
   })
 );
 
 const painter = new Painter();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hi!");
-  res.sendStatus(200);
-});
-
 app.get("/test", (req: Request, res: Response) => {
-  NextPixelsOn(4096, painter);
+  FillPanelPixels(5, painter);
   painter.Clear();
   res.sendStatus(200);
 });
 
 app.get("/text/:text", (req: Request, res: Response) => {
   let text: string = req.params["text"];
-  painter.Sleep(3000);
   PrintOwnText(painter, text);
   painter.Clear();
-  res.sendStatus(200);
-});
-
-app.get("/pixel/:x/:y", (req: Request, res: Response) => {
-  let x: number = Number(req.params["x"]);
-  let y: number = Number(req.params["y"]);
-  painter.PaintPixel(x, y);
   res.sendStatus(200);
 });
 
@@ -50,6 +35,11 @@ app.post("/xinukIteration", (req: Request, res: Response) => {
   const xinukIteration: XinukIteration = ConvertBodyToXinukIteration(req.body);
   painter.Paint(xinukIteration.points);
   console.log("Iteration: ", xinukIteration.iterationNumber);
+  res.sendStatus(200);
+});
+
+app.get("/clear", (req: Request, res: Response) => {
+  painter.Clear();
   res.sendStatus(200);
 });
 
